2f20de1456e0613cb62c681759e25946b64f1ead
HikaruEgashira <s1811320@gmail.com>
2021-06-29 15:29:11 +0900

    refactor lib に分割

---
+++ lib/lib.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cbroglie/mustache"
+	"github.com/russross/blackfriday"
+)
+
+func GenerateHTML(path string, context ...interface{}) string {
+	md, _ := ioutil.ReadFile(path + ".md")
+	mustacheHtml := string(blackfriday.Run(md))
+	html, _ := mustache.Render(mustacheHtml, context...)
+	return html
+}
+
+func Render(w http.ResponseWriter, data string) {
+	tmpl := template.Must(template.ParseFiles("template/md.html"))
+	tmpl.ExecuteTemplate(w, "md", template.HTML(data))
+}
--- routes.go
+++ routes.go
@@ -1,46 +1,30 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
+	"github.com/HikaruEgashira/simple-server/lib"
 	"github.com/HikaruEgashira/simple-server/usecase"
-	"github.com/cbroglie/mustache"
-	"github.com/russross/blackfriday"
 )
-
-func generateHTML(path string) string {
-	out, _ := ioutil.ReadFile(path + ".md")
-	return string(blackfriday.Run(out))
-}
-
-func render(w http.ResponseWriter, data string) {
-	tmpl := template.Must(template.ParseFiles("template/md.html"))
-	tmpl.ExecuteTemplate(w, "md", template.HTML(data))
-}
 
 // シンプルな例
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	output := generateHTML("pages/index")
-	render(w, output)
+	output := lib.GenerateHTML("pages/index")
+	lib.Render(w, output)
 }
 
 // パラメータを利用する例
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-	output := generateHTML("pages/add")
 	a, _ := strconv.Atoi(r.URL.Query().Get("a"))
 	b, _ := strconv.Atoi(r.URL.Query().Get("b"))
-
 	c := usecase.Add(a, b)
 
-	data, _ := mustache.Render(output, map[string]int{"a": a, "b": b, "c": c})
-
-	render(w, data)
+	output := lib.GenerateHTML("pages/add", map[string]int{"a": a, "b": b, "c": c})
+	lib.Render(w, output)
 }
 
 func ReadmeHandler(w http.ResponseWriter, r *http.Request) {
-	output := generateHTML("README")
-	render(w, output)
+	output := lib.GenerateHTML("README")
+	lib.Render(w, output)
 }
